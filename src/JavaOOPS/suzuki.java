0175bdc8ad853190561d81ddeeea0d50da9e95c7
sridharyerragunta <47591905+sridharyerragunta@users.noreply.github.com>
2019-02-13 15:06:56 +0530

    Update suzuki.java
    added stop method

--- src/JavaOOPS/suzuki.java
+++ src/JavaOOPS/suzuki.java
@@ -4,4 +4,8 @@ public class suzuki extends CAR {
   public void start(){
   System.out.println("suszuki start method");	  
   }
+  public void stop(){
+  System.out.println("suszuki stop method");	  
+  }
+
 }
